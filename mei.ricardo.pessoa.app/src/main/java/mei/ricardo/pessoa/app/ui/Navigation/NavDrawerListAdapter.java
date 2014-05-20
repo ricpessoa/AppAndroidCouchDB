1e573626bb3d18fefec3cf619cdad85ec132c21f
linuxrp <linuxrp@hotmail.com>
2014-05-20 16:31:01 +0100

    Custom the Navigation Drawer to show icon, and name add NavDrawerItem and ListAdapter)

--- mei.ricardo.pessoa.app/src/main/java/mei/ricardo/pessoa/app/ui/Navigation/NavDrawerListAdapter.java
+++ mei.ricardo.pessoa.app/src/main/java/mei/ricardo/pessoa/app/ui/Navigation/NavDrawerListAdapter.java
@@ -1,5 +1,3 @@
-package mei.ricardo.pessoa.app.ui.Navigation;
-
 
 import java.util.ArrayList;
 
@@ -13,57 +11,58 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import mei.ricardo.pessoa.app.R;
+import mei.ricardo.pessoa.app.ui.Navigation.NavDrawerItem;
 
 public class NavDrawerListAdapter extends BaseAdapter {
-	
-	private Context context;
-	private ArrayList<NavDrawerItem> navDrawerItems;
-	
-	public NavDrawerListAdapter(Context context, ArrayList<NavDrawerItem> navDrawerItems){
-		this.context = context;
-		this.navDrawerItems = navDrawerItems;
-	}
 
-	@Override
-	public int getCount() {
-		return navDrawerItems.size();
-	}
+    private Context context;
+    private ArrayList<NavDrawerItem> navDrawerItems;
 
-	@Override
-	public Object getItem(int position) {		
-		return navDrawerItems.get(position);
-	}
+    public NavDrawerListAdapter(Context context, ArrayList<NavDrawerItem> navDrawerItems){
+        this.context = context;
+        this.navDrawerItems = navDrawerItems;
+    }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+    @Override
+    public int getCount() {
+        return navDrawerItems.size();
+    }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		if (convertView == null) {
+    @Override
+    public Object getItem(int position) {
+        return navDrawerItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
             LayoutInflater mInflater = (LayoutInflater)
                     context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
             convertView = mInflater.inflate(R.layout.drawer_list_item, null);
         }
-         
+
         ImageView imgIcon = (ImageView) convertView.findViewById(R.id.icon);
         TextView txtTitle = (TextView) convertView.findViewById(R.id.title);
         TextView txtCount = (TextView) convertView.findViewById(R.id.counter);
-         
-        imgIcon.setImageResource(navDrawerItems.get(position).getIcon());        
+
+        imgIcon.setImageResource(navDrawerItems.get(position).getIcon());
         txtTitle.setText(navDrawerItems.get(position).getTitle());
-        
+
         // displaying count
         // check whether it set visible or not
         if(navDrawerItems.get(position).getCounterVisibility()){
-        	txtCount.setText(navDrawerItems.get(position).getCount());
+            txtCount.setText(navDrawerItems.get(position).getCount());
         }else{
-        	// hide the counter view
-        	txtCount.setVisibility(View.GONE);
+            // hide the counter view
+            txtCount.setVisibility(View.GONE);
         }
-        
+
         return convertView;
-	}
+    }
 
 }
